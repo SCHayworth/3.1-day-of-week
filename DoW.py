73abc21f22041b9f0df56266d156ea207ff4bad5
Shaun Hayworth <thismoderndeath@gmail.com>
2019-10-01 16:28:41 -0700

    Added a loop to run the program multiple times

--- DoW.py
+++ DoW.py
@@ -6,27 +6,41 @@
 # Prompts user to enter an integer between 1 and 7, and displays a day of the week
 # corresponding with the input.
 
-# Prompt user for input and store it in the day_of_week variable
-day_of_week = int(input('Please enter a number between 1 and 7: '))
+# Initialize the check_day boolean variable and set it to True
+check_day = True
 
-# To be done:
-#   Check for valid integer
-#   Check for valid range
+while check_day == True:
+    # Prompt user for input and store it in the day_of_week variable
+    day_of_week = int(input('Please enter a number between 1 and 7: '))
 
-# Display name of day corresponding with input.
-if day_of_week == 1:
-    print('Monday')
-elif day_of_week == 2:
-    print('Tuesday')
-elif day_of_week == 3:
-    print('Wednesday')
-elif day_of_week == 4:
-    print('Thursday')
-elif day_of_week == 5:
-    print('Friday')
-elif day_of_week == 6:
-    print('Saturday')
-elif day_of_week == 7:
-    print('Sunday')
+    # To be done:
+    #   Check for valid integer
+    #   Check for valid range
+
+    # Display name of day corresponding with input, or an error message if input is inappropriate.
+    if day_of_week == 1:
+        print('Monday')
+    elif day_of_week == 2:
+        print('Tuesday')
+    elif day_of_week == 3:
+        print('Wednesday')
+    elif day_of_week == 4:
+        print('Thursday')
+    elif day_of_week == 5:
+        print('Friday')
+    elif day_of_week == 6:
+        print('Saturday')
+    elif day_of_week == 7:
+        print('Sunday')
+    else:
+        print('That is not a valid day.')
+          
+    # Ask user if they'd like to enter another day.
+    another_day = char(input('Would you like to enter a new day (y/n)? '))
+    
+    # Run the program again if yes, or else set check_day to False.
+    if another_day == n or N:
+        check_day = False
 else:
-    print('That is not a valid day.')
+    print('Goodbye!)
+        
