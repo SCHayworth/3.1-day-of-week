78ad8b56872c71aff4bd0be458b74cab9dd06194
Shaun Hayworth <thismoderndeath@gmail.com>
2019-10-01 19:01:36 -0700

    First draft of the main function

--- DoW.py
+++ DoW.py
@@ -1,4 +1,4 @@
-# 3-1 Day of the Week Program
+# 3-1 Day of the Week Program v. 2
 # by Shaun Hayworth
 # CIS 2
 # 2019-10-01
@@ -6,17 +6,14 @@
 # Prompts user to enter an integer between 1 and 7, and displays a day of the week
 # corresponding with the input.
 
-# Initialize the check_day variable and set it to 1
-check_day = 1
+# This version of the program is built with the main code in a function, so that the program can be run multiple times without
+# having to be re-initiated.
 
-# Loop the program as long as check_day is 1, or else end the program.
-while check_day == 1:
+# Define the function, main(), which prompts the user for a number between 1 and 7, and prints the corresponding day to the screen.
+# If the response is out of range, an error message is displayed.
+def main():
     # Prompt user for input and store it in the day_of_week variable
     day_of_week = int(input('Please enter a number between 1 and 7: '))
-
-    # To be done:
-    #   Check for valid integer
-    #   Check for valid range
 
     # Display name of day corresponding with input, or an error message if input is inappropriate.
     if day_of_week == 1:
@@ -35,13 +32,8 @@ while check_day == 1:
         print('Sunday')
     else:
         print('That is not a valid day.')
+        
+# TEST call the function main()
+main()
           
-    # Ask user if they'd like to enter another day.
-    another_day = str(input('Would you like to enter a new day (y/n)? '))
-    
-    # Run the program again if yes, or else set check_day to False.
-    if another_day == 'n' or 'N':
-        check_day = 0
-else:
-    print('Goodbye!')
         
